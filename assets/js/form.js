31e4896e73dedb823bd0810e4a45eb5e212480f4
=pankajapplication <=>
2024-06-05 16:26:40 +0530

    hello

---
+++ assets/js/form.js
@@ -0,0 +1,31 @@
+let form = document.getElementById('formValidate');
+let fName,lName,uEmail,uEmailFormat,uMob;
+fName = document.getElementById('fName');
+    console.log(fName);
+    lName = document.getElementById('lName');
+    uEmail = document.getElementById('uEmail');
+    uMob = document.getElementById('uMob');
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    if(fName.value === ''){
+        fName.nextElementSibling.textContent = "Enter First Name";
+    }else{
+        fName.nextElementSibling.textContent = "";
+    }
+    if(lName.value === ''){
+        lName.nextElementSibling.textContent = "Enter Last Name";
+    }else{
+        lName.nextElementSibling.textContent = "";
+    }
+    if(uEmail.value === ''){
+        uEmail.nextElementSibling.textContent = "Enter Your Email";
+    }else{
+        uEmail.nextElementSibling.textContent = "";
+    }
+    if(uMob.value === ''){
+        uMob.nextElementSibling.textContent = "Enter Mobile Number";
+    }else{
+        uMob.nextElementSibling.textContent = "";
+    }
+    return false
+})
